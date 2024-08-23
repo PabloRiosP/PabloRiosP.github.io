ed052ecbb65f894221d78788805b454e9dab1474
Pablo Ríos <pablo_rios_p@protonmail.com>
2024-08-23 00:27:53 -0400

    Habilitado el formulario y removida la advertencia

---
+++ static/js/contact.js
@@ -0,0 +1,23 @@
+// Inicializando el servicio
+emailjs.init("DUR0SD1U5BvgXKl2n");
+
+const btn = document.getElementById("submit-button");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    btn.value = "Enviando...";
+
+    emailjs.sendForm("service_61926pv", "template_q3npk9q", this).then(
+      () => {
+        btn.value = "Enviar";
+        alert("Mensaje enviado con éxito");
+      },
+      (error) => {
+        btn.value = "Enviar";
+        alert(`Hubo un error al enviar el mensaje: ${error}`);
+      }
+    );
+  });
