e673190e8af9196620e40c20df5cad73ab0fc27c
Pablo Ríos <pablo_rios_p@protonmail.com>
2024-08-25 14:43:20 -0400

    Primeros estilos reales
    Agregada fuente externa "Red Hat Display" desde Google Fonts (OFL).
    
    Agregado icono de barras para responsividad de barra de navegación en
    dispositivos móviles.
    
    Configurados estilos en barra de navegación responsiva.
    
    Configuración general de estilos, simple pero funcional, centrada en
    conseguir una experiencia de usuario fluida.
    
    Aún falta configurar la paleta de colores oscura, agregar las clases
    nuevas a las otras páginas, y antes de eso terminar la hoja de estilos.
    Espero tener subida la versión final del CSS del tema claro hoy,
    quedando únicamente los colores oscuros y la adaptación al resto del
    sitio a más tardar para mañana. Queda bastante trabajo.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,21 @@
-console.log("this script is unused for now.");
+// Ajuste de la barra de navegación
+document.getElementById("navbar-mobile-bars").onclick = () => {
+  let element = document.getElementById("navbar");
+  let style = window.getComputedStyle(element);
+  let display = style.getPropertyValue("display");
+  if (display == "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+};
+
+// Corrección de posible error tras cambio de tamaño, en el que la barra desaparece.
+window.addEventListener("resize", () => {
+    let element = document.getElementById("navbar");
+    if (window.innerWidth > 600) {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+});
