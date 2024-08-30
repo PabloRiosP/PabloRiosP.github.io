7d3ea4c5133f2a24c454352748169e980894a1a0
Pablo Ríos <pablo_rios_p@protonmail.com>
2024-08-29 22:58:02 -0400

    Estilos de formulario y correcciones

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,18 @@
+// Desactivar transiciones al inicio
+function generalClassAdd(css_class) {
+  document.body.classList.add(css_class);
+}
+
+// Reactivar transiciones
+function generalClassRemove(css_class, timeout) {
+  // Forzar un reflujo para asegurarse de que las transiciones están desactivadas
+  document.body.offsetHeight;
+
+  setTimeout(() => {
+    document.body.classList.remove(css_class);
+  }, timeout);
+}
+
 // Ajuste de la barra de navegación
 document.getElementById("navbar-mobile-bars").onclick = () => {
   let element = document.getElementById("navbar");
@@ -22,9 +37,17 @@ window.addEventListener("resize", () => {
 
 // Manejo del tema claro y oscuro
 document.getElementById("theme-btn").onclick = () => {
+  generalClassAdd('theme-toggle-transition');
   const isDark = document.body.classList.toggle('dark-theme');
   const newTheme = isDark ? 'dark' : 'light';
   localStorage.setItem('theme', newTheme);
   document.getElementById("theme-icon").classList.toggle('fa-sun', isDark);
   document.getElementById("theme-icon").classList.toggle('fa-moon', !isDark);
+  generalClassRemove('theme-toggle-transition', 260);
 }
+
+generalClassAdd('no-transition');
+
+window.addEventListener("load", () => {
+  generalClassRemove('no-transition', 100);
+});
