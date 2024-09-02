ca7b25761a96af3e1de0a13cae5c403e78f0a252
Pablo Ríos <pablo_rios_p@protonmail.com>
2024-09-01 22:13:54 -0400

    Reparando problema en formulario de contacto

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,25 +1,28 @@
-// Para el formulario de contacto.
-// Envía el contenido a un medio especificado en el sericio
-// Inicializando el servicio
-emailjs.init("DUR0SD1U5BvgXKl2n");
-
-const btn = document.getElementById("submit-button");
-
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    btn.value = "Enviando...";
-
-    emailjs.sendForm("service_61926pv", "template_q3npk9q", this).then(
-      () => {
-        btn.value = "Enviar";
-        alert("Mensaje enviado con éxito");
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+  
+  var url = 'pabloriosform.infinitefreeapp.com';
+  
+  fetch(url, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
       },
-      (error) => {
-        btn.value = "Enviar";
-        alert(`Hubo un error al enviar el mensaje: ${error}`);
-      }
-    );
+      body: JSON.stringify({
+          name: name,
+          email: email,
+          message: message
+      })
+  })
+  .then(response => response.text())
+  .then(result => {
+      alert(result);
+  })
+  .catch(error => {
+      alert('Hubo un problema al enviar el mensaje: ' + error.message);
   });
+});
