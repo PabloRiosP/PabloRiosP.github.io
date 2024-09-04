57e50851fc937391d0b0aed46c67d94baee4f915
Pablo Ríos Pérez <pablo_rios_p@protonmail.com>
2024-09-04 19:41:54 -0400

    Añadiendo keepalive a contact + correccion footer

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,3 +1,54 @@
+let typingTimer;
+let keepAliveTimer;
+let keepAliveCounter = 0;
+const inactivityLimit = 30 * 1000;
+const keepAliveInterval = 290 * 1000;
+const maxKeepAlives = 3;
+
+function resetTypingTimer() {
+    clearTimeout(typingTimer);
+    clearTimeout(keepAliveTimer);
+
+    if (keepAliveCounter < maxKeepAlives) {
+        typingTimer = setTimeout(stopKeepAlive, inactivityLimit);
+        keepAliveTimer = setTimeout(keepAlive, keepAliveInterval);
+    }
+}
+
+function stopKeepAlive() {
+    clearTimeout(keepAliveTimer);
+    console.log("Inactividad detectada o límite de keep alive alcanzado, deteniendo conexión.");
+}
+
+function keepAlive() {
+    if (keepAliveCounter < maxKeepAlives) {
+        fetch("https://clean-organic-tiglon.glitch.me/ping", {
+            method: "GET",
+            keepalive: true
+        }).then(response => {
+            if (!response.ok) {
+                console.log("Fallo en el keep alive");
+            } else {
+                keepAliveCounter++;
+                console.log("Conexión mantenida, número de keep alive enviados:", keepAliveCounter);
+                resetTypingTimer();
+            }
+        }).catch(error => {
+            console.log("Error en el keep alive:", error);
+        });
+    } else {
+        stopKeepAlive();
+    }
+}
+
+// Escuchar eventos de tipeo en los campos del formulario
+document.querySelectorAll('#contact-form input, #contact-form textarea').forEach(input => {
+    input.addEventListener('input', resetTypingTimer);
+});
+
+// Iniciar el primer ciclo de keep alive cuando se carga la página
+resetTypingTimer();
+
 document.getElementById("contact-form").onsubmit = (event) => {
   event.preventDefault();
 
@@ -9,7 +60,7 @@ document.getElementById("contact-form").onsubmit = (event) => {
   let email = emailField.value;
   let message = messageField.value;
 
-  fetch("https://clean-organic-tiglon.glitch.me", {
+  fetch("https://clean-organic-tiglon.glitch.me/ping", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -31,3 +82,4 @@ document.getElementById("contact-form").onsubmit = (event) => {
       alert("Hubo un problema al enviar el mensaje: " + error.message);
     });
 };
+
